3c4cbdebf9df0ef00b3ea6beccf4ab7d1889ac0b
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-06-15 16:39:01 -0700

    updated component methods

--- src/components/singleUser.js
+++ src/components/singleUser.js
@@ -2,10 +2,15 @@ import React from 'react'
 
 class SingleUser extends React.Component {
     render() {
+
+        const handleSetUserId = (e) => {
+            this.props.setUserId(e)
+        }
+
         return (
             <div>
-                <h1 className='userId' id={this.props.elem.id}>User Id: {this.props.elem.id}</h1>
-                <h1 className='post'>{this.props.elem.name}</h1>
+                <h1 className='userIds'>User Id: {this.props.elem.id}</h1>
+                <h1 className='userNames'  id={this.props.elem.id} onClick={handleSetUserId}>{this.props.elem.name}</h1>
             </div>
         )
     }

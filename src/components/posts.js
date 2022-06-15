7de6b7a2f42317ecdcb4f97a51fde75e16bc2ef6
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-06-15 13:11:35 -0700

    Built basic posts component

---
+++ src/components/posts.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PostItem from './postItem'
+
+class Posts extends React.Component {
+    render() {
+        return(
+            this.props.posts.map((elem) => {
+                return <PostItem elem={elem} key={elem.id} setSinglePost={this.props.setSinglePost} />
+            })
+        )
+    }
+}
+
+export default Posts

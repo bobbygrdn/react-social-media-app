7ab09e4d5c5bc01fcee7d6952f93c40830099f7d
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-06-15 14:35:37 -0700

    built user id element into component

--- src/components/postItem.js
+++ src/components/postItem.js
@@ -8,8 +8,11 @@ class PostItem extends React.Component{
         }
 
         return (
+            <div className='postContainer'>
+            <h1 className='userId'>User Id: {this.props.elem.userId}</h1>
             <h1 className='postItem' id={this.props.elem.id} onClick={handleSetSinglePost}>{this.props.elem.title}
             </h1>
+            </div>
         )
     }
 }

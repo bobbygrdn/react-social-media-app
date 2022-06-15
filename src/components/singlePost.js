cfbd5efe2891a5b9c204acfe9693285480476027
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-06-15 14:18:35 -0700

    changed class name for more styling control

--- src/components/singlePost.js
+++ src/components/singlePost.js
@@ -4,7 +4,7 @@ class SinglePost extends React.Component {
     render() {
         return (
             <div>
-                <h1 className="title">{this.props.singlePost.title}</h1>
+                <h1 className="postTitle">{this.props.singlePost.title}</h1>
                 <p className='singlePost' id='{this.props.singlePost.id'>{this.props.singlePost.body}</p>
             </div>
         )

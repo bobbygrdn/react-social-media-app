1d69305c46ad89d43dcae8a7ccca9f524bbf7f46
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-06-15 16:38:54 -0700

    Updated component methods

--- src/components/users.js
+++ src/components/users.js
@@ -3,9 +3,10 @@ import SingleUser from './singleUser'
 
 class Users extends React.Component {
     render() {
+
         return(
             this.props.users.map((elem) => {
-                return <SingleUser elem={elem} key={elem.id} />
+                return <SingleUser elem={elem} key={elem.id} setUserId={this.props.setUserId} />
             })
         )
     }

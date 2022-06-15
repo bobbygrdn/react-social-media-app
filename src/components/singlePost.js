06a307ea1aca9422b6537640f5aa78d432e6132e
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-06-15 15:14:35 -0700

    Updated component to render userid

--- src/components/singlePost.js
+++ src/components/singlePost.js
@@ -2,10 +2,16 @@ import React from 'react'
 
 class SinglePost extends React.Component {
     render() {
+
+        const handleSetUserId = (e) => {
+            this.props.setUserId(e)
+        }
+
         return (
             <div>
+                <h1 className='userId' id={this.props.singlePost.userId} onClick={handleSetUserId}>User Id: {this.props.singlePost.userId}</h1>
                 <h1 className="postTitle">{this.props.singlePost.title}</h1>
-                <p className='singlePost' id='{this.props.singlePost.id'>{this.props.singlePost.body}</p>
+                <p className='singlePost' id={this.props.singlePost.id}>{this.props.singlePost.body}</p>
             </div>
         )
     }

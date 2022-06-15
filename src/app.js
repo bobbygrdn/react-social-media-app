83ff84515894727df7fbb8f3c0953ded7e1b8ec0
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-06-15 12:21:19 -0700

    Changed website icon

--- src/app.js
+++ src/app.js
@@ -1,7 +1,30 @@
 import React from 'react'
 
 class App extends React.Component {
+    constructor(props) {
+        super(props)
 
+        this.state = {
+            posts: null,
+            singlePost: null,
+            loading: true,
+            loadingMessage: 'Data is currently being received from server. Please wait.',
+            User: null,
+            Users: null,
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(data => console.log(data))
+    }
+
+    render() {
+        return (
+            <h1>Hello World!</h1>
+        )
+    }
 }
 
 export default App

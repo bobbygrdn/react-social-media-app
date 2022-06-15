c43c80a8f7ee86cb3ab8890ed6aacd597c62f40e
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-06-15 16:50:15 -0700

    Updated component classes for more styling control

--- src/components/userProfile.js
+++ src/components/userProfile.js
@@ -4,19 +4,18 @@ class UserProfile extends React.Component {
     render() {
         return (
             <div className='profile'>
-                <h1>{this.props.user.name}</h1>
-                <h2>Username: {this.props.user.username}</h2>
-                <h2>Email: {this.props.user.email}</h2>
-                <h2>Address: </h2>
-                <h3>{this.props.user.address.street}</h3>
-                <h3>{this.props.user.address.suite}</h3>
-                <h3>{this.props.user.address.city}</h3>
-                <h3>{this.props.user.address.zipcode}</h3>
-                <h2>Phone Number: {this.props.user.phone}</h2>
-                <h2>Website: {this.props.user.website}</h2>
-                <h2>Company: </h2>
-                <h3>{this.props.user.company.name}</h3>
-                <h3>{this.props.user.company.catchPhrase}</h3>
+                <h1 className='profileInfo'>{this.props.user.name}</h1>
+                <h2 className='profileInfo'>Username: {this.props.user.username}</h2>
+                <h2 className='profileInfo'>Email: {this.props.user.email}</h2>
+                <h2 className='profileInfo'>Address: </h2>
+                <h3 className='profileInfo'>{this.props.user.address.street}</h3>
+                <h3 className='profileInfo'>{this.props.user.address.suite}</h3>
+                <h3 className='profileInfo'>{this.props.user.address.city}</h3>
+                <h3 className='profileInfo'>{this.props.user.address.zipcode}</h3>
+                <h2 className='profileInfo'>Phone Number: {this.props.user.phone}</h2>
+                <h2 className='profileInfo'>Website: {this.props.user.website}</h2>
+                <h1 className='profileInfo'>{this.props.user.company.name}</h1>
+                <h2 className='profileInfo'> The leading {this.props.user.company.catchPhrase}</h2>
             </div>
         )
     }

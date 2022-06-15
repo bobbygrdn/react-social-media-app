78a04a14d602e9270fbc175e35f135e6534dbcef
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-06-15 15:14:58 -0700

    updated component to handle userid method

--- src/components/postItem.js
+++ src/components/postItem.js
@@ -7,9 +7,13 @@ class PostItem extends React.Component{
             this.props.setSinglePost(e)
         }
 
+        const handleSetUserId = (e) => {
+            this.props.setUserId(e)
+        }
+
         return (
             <div className='postContainer'>
-            <h1 className='userId'>User Id: {this.props.elem.userId}</h1>
+            <h1 className='userId' id={this.props.elem.userId} onClick={handleSetUserId}>User Id: {this.props.elem.userId}</h1>
             <h1 className='postItem' id={this.props.elem.id} onClick={handleSetSinglePost}>{this.props.elem.title}
             </h1>
             </div>
